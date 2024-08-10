271abc8802206d45cecb96338962a290d940784a
Juan Zhang <tzzhangjuan@gmail.com>
2024-08-09 23:05:38 -0400

    Update RNA-seq analysis_section5, gene set enrichment analysis (GSEA).R

--- RNA-seq analysis/RNA-seq analysis_section5, gene set enrichment analysis (GSEA).R
+++ RNA-seq analysis/RNA-seq analysis_section5, gene set enrichment analysis (GSEA).R
@@ -12,7 +12,7 @@
   ## 4.RNA-seq from ASO-L1 KD in PXGL hESCs
   ## 5.RNA-seq from EZH2i in RSeT+DT hESCs
   ## 6.RNA-seq from ASO-L1 and TPRX1-siRNA transfected co-KD in RSeT+DT hESCs.
-  ## 7.RNA-seq from Collinson et al 2016, Cell Reports_GSE76626_RNA-Seq (data process is described in the STAR METHOD, and 
+  ## 7.RNA-seq from Collinson et al 2016, Cell Reports_GSE76626_RNA-Seq (normalized counts table "GSE76626_EZH2null_WT,cutoff=0.2,Norm_counts.txt" is analyzed following RNA-seq analysis methods in this paper, and is included in the repository. 
 
 
 library(fgsea)
